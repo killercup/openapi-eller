9e2839826d0aa7fb6f7d70ae65253b1c70bdcdaa
Pascal Hertleif <pascal@technocreatives.com>
2019-05-23 17:00:46 +0200

    Add some features to handlebars
    It's a full blown programming language now, I'm sorry

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,34 @@ import { resolveTarget } from "./targets"
 import { GeneratorVisitor, ModelGenerator } from "./visitor"
 // import logger from "winston"
 
+import Handlebars from "handlebars"
+Handlebars.registerHelper({
+  eq(v1, v2) {
+    return v1 === v2
+  },
+  ne(v1, v2) {
+    return v1 !== v2
+  },
+  lt(v1, v2) {
+    return v1 < v2
+  },
+  gt(v1, v2) {
+    return v1 > v2
+  },
+  lte(v1, v2) {
+    return v1 <= v2
+  },
+  gte(v1, v2) {
+    return v1 >= v2
+  },
+  and() {
+    return Array.prototype.slice.call(arguments).every(Boolean)
+  },
+  or() {
+    return Array.prototype.slice.call(arguments, 0, -1).some(Boolean)
+  }
+})
+
 function generateSecuritySchemes(
   target: Target
 ): {}[] {
